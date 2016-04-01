1eb7a0769a879441f593f9a4c65478a5bf66d918
deansunny <huangdi.hdu@gmail.com>
2016-04-01 22:41:00 +0800

    debug

--- tools/python/splh5.py
+++ tools/python/splh5.py
@@ -3,21 +3,20 @@ import numpy as np
 import h5py
  
 def split_HDF5_val_file(h5_path):
-        
 	input_path = os.path.join(h5_path, 'val', 'val.h5')
         print input_path
 	h5_file = h5py.File(input_path)
-    save_path = os.path.join(h5_path, 'val')
+        save_path = os.path.join(h5_path, 'val')
 	image_num = np.shape(h5_file['data'])[0]
 	f1 = h5py.File(os.path.join(save_path, 'val1.h5'), 'w')
 	f2 = h5py.File(os.path.join(save_path, 'val2.h5'), 'w')
         f3 = h5py.File(os.path.join(save_path, 'val3.h5'), 'w')
 	
-	f1.create_dataset('data', (12871, 3, 256, 256), dtype=np.float32)
+	f1.create_dataset('data', (12871, 3, 227, 227), dtype=np.float32)
 	f1.create_dataset('label', (12871, 10), dtype=np.float32)
-	f2.create_dataset('data', (12872, 3, 256, 256), dtype=np.float32)
+	f2.create_dataset('data', (12872, 3, 227, 227), dtype=np.float32)
 	f2.create_dataset('label', (12872, 10), dtype=np.float32)
-	f3.create_dataset('data', (12872, 3, 256, 256), dtype=np.float32)
+	f3.create_dataset('data', (12872, 3, 227, 227), dtype=np.float32)
 	f3.create_dataset('label', (12872, 10), dtype=np.float32)	
 
 
@@ -25,12 +24,12 @@ def split_HDF5_val_file(h5_path):
 		if ind < 12871:
                         f1['data'][ind] = h5_file['data'][ind]
                         f1['label'][ind] = h5_file['label'][ind]
-		elif ind >= 12871 and ind < 25742:
-                        f2['data'][ind] = h5_file['data'][ind]
-                        f2['label'][ind] = h5_file['label'][ind]
+		elif ind >= 12871 and ind < 25743:
+                        f2['data'][ind-12871] = h5_file['data'][ind]
+                        f2['label'][ind-12871] = h5_file['label'][ind]
 		else:
-                        f3['data'][ind] = h5_file['data'][ind]
-                        f3['label'][ind] = h5_file['label'][ind]
+                        f3['data'][ind-25743] = h5_file['data'][ind]
+                        f3['label'][ind-25743] = h5_file['label'][ind]
 		if ind % 10000 == 0 and ind != 0:
 			print '{} done.'.format(ind)
 
@@ -38,7 +37,6 @@ def split_HDF5_val_file(h5_path):
 	f2.close()
 	f3.close()
 	h5_file.close()
-	#print np.shape(f1['data']), np.shape(f2['data']), np.shape(f3['data'])
 
 def split_HDF5_test_file(h5_path):
         input_path = os.path.join(h5_path, 'test', 'test.h5')
@@ -58,26 +56,26 @@ def split_HDF5_test_file(h5_path):
         image_num = np.shape(h5_file['data'])[0]
 
         
-        f1.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+        f1.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f1.create_dataset('label', (5792, 10), dtype=np.float32)
-	f2.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f2.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f2.create_dataset('label', (5792, 10), dtype=np.float32)
-	f3.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f3.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f3.create_dataset('label', (5792, 10), dtype=np.float32)
-	f4.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f4.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f4.create_dataset('label', (5792, 10), dtype=np.float32)
-	f5.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f5.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f5.create_dataset('label', (5792, 10), dtype=np.float32)
-	f6.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f6.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f6.create_dataset('label', (5792, 10), dtype=np.float32)
-	f7.create_dataset('data', (5792, 3, 256, 256), dtype=np.float32)
+	f7.create_dataset('data', (5792, 3, 227, 227), dtype=np.float32)
 	f7.create_dataset('label', (5792, 10), dtype=np.float32)
-	f8.create_dataset('data', (5793, 3, 256, 256), dtype=np.float32)
+	f8.create_dataset('data', (5793, 3, 227, 227), dtype=np.float32)
 	f8.create_dataset('label', (5793, 10), dtype=np.float32)
 	
-	f9.create_dataset('data', (5793, 3, 256, 256), dtype=np.float32)
+	f9.create_dataset('data', (5793, 3, 227, 227), dtype=np.float32)
 	f9.create_dataset('label', (5793, 10), dtype=np.float32)
-	f10.create_dataset('data', (5793, 3, 256, 256), dtype=np.float32)
+	f10.create_dataset('data', (5793, 3, 227, 227), dtype=np.float32)
 	f10.create_dataset('label', (5793, 10), dtype=np.float32)
 
         for ind in xrange(image_num):
@@ -156,26 +154,26 @@ def split_HDF5_train_file(h5_path):
         image_num = np.shape(h5_file['data'])[0]
 
         
-        f1.create_dataset('data', (9653, 3, 256, 256), dtype=np.float32)
+        f1.create_dataset('data', (9653, 3, 227, 227), dtype=np.float32)
 	f1.create_dataset('label', (9653, 10), dtype=np.float32)
-	f2.create_dataset('data', (9653, 3, 256, 256), dtype=np.float32)
+	f2.create_dataset('data', (9653, 3, 227, 227), dtype=np.float32)
 	f2.create_dataset('label', (9653, 10), dtype=np.float32)
-	f3.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f3.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f3.create_dataset('label', (9654, 10), dtype=np.float32)
-	f4.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f4.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f4.create_dataset('label', (9654, 10), dtype=np.float32)
-	f5.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f5.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f5.create_dataset('label', (9654, 10), dtype=np.float32)
-	f6.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f6.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f6.create_dataset('label', (9654, 10), dtype=np.float32)
-	f7.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f7.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f7.create_dataset('label', (9654, 10), dtype=np.float32)
-	f8.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f8.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f8.create_dataset('label', (9654, 10), dtype=np.float32)
 	
-	f9.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f9.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f9.create_dataset('label', (9654, 10), dtype=np.float32)
-	f10.create_dataset('data', (9654, 3, 256, 256), dtype=np.float32)
+	f10.create_dataset('data', (9654, 3, 227, 227), dtype=np.float32)
 	f10.create_dataset('label', (9654, 10), dtype=np.float32)
 
         for ind in xrange(image_num):
@@ -239,6 +237,6 @@ def split_HDF5_train_file(h5_path):
 
 if __name__ == '__main__':    
     path = '/home/u514/DTask/data/AVA'
-    #split_HDF5_val_file(path)
+    split_HDF5_val_file(path)
     split_HDF5_test_file(path)
-    #split_HDF5_train_file(path)
+    split_HDF5_train_file(path)
